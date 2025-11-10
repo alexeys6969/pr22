c21f6c9f71ccd05550f41dfa89f4a9b78e77eab6
alexeys6969 <alexshashin.2023@gmail.com>
2025-11-10 19:27:23 +0500

    Разработанное окно MainWindow. Шашин

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +22,34 @@ namespace Phonebook_Shashin
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static Connection connect;
+        public static Pages.Main main;
         public MainWindow()
         {
             InitializeComponent();
+            connect = new Connection();
+            connect.LoadData(Connection.tabels.users);
+            connect.LoadData(Connection.tabels.calls);
+            main = new Pages.Main();
+            OpenPageMain();
+        }
+
+        public void OpenPageMain()
+        {
+            DoubleAnimation opgridAnimation = new DoubleAnimation();
+            opgridAnimation.From = 1;
+            opgridAnimation.To = 0;
+            opgridAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            opgridAnimation.Completed += delegate
+            {
+                frame.Navigate(main);
+                DoubleAnimation opgrisdAnimation = new DoubleAnimation();
+                opgrisdAnimation.From = 0;
+                opgrisdAnimation.To = 1;
+                opgrisdAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                frame.BeginAnimation(Frame.OpacityProperty, opgrisdAnimation);
+            };
+            frame.BeginAnimation(Frame.OpacityProperty, opgridAnimation);
         }
     }
 }
